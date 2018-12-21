6c2b4094ae507558326a6e19cbba4466c9812cd1
Victor Presumido <victor.presumido@gmail.com>
2018-12-21 09:34:19 +0100

    Add more queries to related to bills

--- app/infrastructure/persistence/queries/BillQueries.scala
+++ app/infrastructure/persistence/queries/BillQueries.scala
@@ -1,23 +1,46 @@
 package infrastructure.persistence.queries
 
+import java.sql.Date
+
+import doobie.free.connection.ConnectionIO
 import doobie.util.query.Query0
-
 import doobie.implicits._
-
 import models.Bill
 
 trait BillQueries {
 
-  def selectAll: Query0[Bill] = {
+  def selectAllBills: Query0[Bill] = {
     sql"""
       SELECT * from bills
-      """.query
+    """.query
   }
 
-  def selectById(id: Int): Query0[Bill] = {
+  def selectBillById(id: Int): Query0[Bill] = {
     sql"""
       SELECT * from bills
       WHERE id = $id
-      """.query[Bill]
+    """.query[Bill]
+  }
+
+  def selectBillByPurchaseDate(purchaseDate: Date): Query0[Bill] = {
+    sql"""
+      SELECT * from bills
+      WHERE purchase_date >= $purchaseDate
+    """.query[Bill]
+  }
+
+  def selectBillBetweenPurchaseDates(from: Date, to: Date): Query0[Bill] = {
+    sql"""
+      SELECT * from bills
+      WHERE purchase_date >= $from
+       AND
+       purchase_date <= $to
+    """.query[Bill]
+  }
+
+  def insertBill(text: String, price: BigDecimal, purchaseDate: Date, location: String): ConnectionIO[Bill] = {
+    sql"insert into bills (text, price, purchase_date, location) values ($text, $price, $purchaseDate, $location)"
+      .update
+      .withUniqueGeneratedKeys("id", "text", "price", "purchase_date","location")
   }
 }
