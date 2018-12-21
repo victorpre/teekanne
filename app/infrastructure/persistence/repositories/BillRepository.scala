43de2f73945a6f88f4f1b9f4783334935292c61b
Victor Presumido <victor.presumido@gmail.com>
2018-12-21 09:35:14 +0100

    Implement bill queries in Repo

--- app/infrastructure/persistence/repositories/BillRepository.scala
+++ app/infrastructure/persistence/repositories/BillRepository.scala
@@ -1,5 +1,7 @@
 package infrastructure.persistence.repositories
 
+import java.sql.Date
+
 import infrastructure.DbConfiguration
 import infrastructure.persistence.queries.BillQueries
 import javax.inject.Inject
@@ -9,8 +11,11 @@ import play.api.db.Database
 import scala.concurrent.{ExecutionContext, Future}
 
 trait BillRepository {
-  def findById(id: Int): Future[Bill]
-  def getAll: Future[List[Bill]]
+  def getBillById(id: Int): Future[Option[Bill]]
+  def getAllBills: Future[List[Bill]]
+  def getBillsFrom(date: Date): Future[List[Bill]]
+  def getBillsBetween(from: Date, to: Date): Future[List[Bill]]
+  def addBill(text: String, price: BigDecimal, purchaseDate: Date, location: String): Future[Bill]
 
 }
 
@@ -21,13 +26,26 @@ class BillRepositoryImpl @Inject()(db: Database)
   import cats.effect.IO
 
   override val cs = IO.contextShift(ec)
+
   val xa = transactor(db, ec)
 
-  override def findById(id: Int): Future[Bill] = {
-    selectById(id).stream.compile.toList.transact(xa).unsafeToFuture().map(_.head)
+  override def getBillById(id: Int): Future[Option[Bill]] = {
+    selectBillById(id).option.transact(xa).unsafeToFuture()
   }
 
-  override def getAll: Future[List[Bill]] = {
-    selectAll.stream.compile.to[List].transact(xa).unsafeToFuture()
+  override def getAllBills: Future[List[Bill]] = {
+    selectAllBills.stream.compile.to[List].transact(xa).unsafeToFuture()
+  }
+
+  override def getBillsFrom(date: Date): Future[List[Bill]] = {
+    selectBillByPurchaseDate(date).stream.compile.toList.transact(xa).unsafeToFuture()
+  }
+
+  override def getBillsBetween(from: Date,to: Date): Future[List[Bill]] = {
+    selectBillBetweenPurchaseDates(from, to).stream.compile.toList.transact(xa).unsafeToFuture()
+  }
+
+  override def addBill(text: String, price: BigDecimal, purchaseDate: Date, location: String): Future[Bill] = {
+    insertBill(text, price, purchaseDate, location).transact(xa).unsafeToFuture()
   }
 }
