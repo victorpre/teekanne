492a329aa735776d96cf7ae10f4c2e90738bbf3e
Victor Presumido <victor.presumido@gmail.com>
2019-01-03 00:20:44 +0100

    Fix tests

--- test/ExpenseControllerSpec.scala
+++ test/ExpenseControllerSpec.scala
@@ -6,24 +6,22 @@ import infrastructure.api.dto.ExpenseDto
 import infrastructure.persistence.repositories.ExpenseRepository
 import models.Expense
 import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
 import org.scalamock.scalatest.MockFactory
 import play.api.libs.json.Json
 
-
 import scala.concurrent.Future
 
 
-class ExpenseControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with MockFactory {
+class ExpenseControllerSpec extends PlaySpec with MockFactory {
   "ExpenseController#getExpense" should {
     "respond with OK to a valid request" in {
       val repo = stub[ExpenseRepository]
-      val controller = new ExpenseController(repo, stubControllerComponents())
       val expense = Expense(1,"Desc",10.0,LocalDate.now(),"Rewe")
       (repo.getExpenseById(_: Int)).when(*).returns(Future.successful(Some(expense)))
 
+      val controller = new ExpenseController(stubControllerComponents(), repo)
       val home = controller.getExpense(1).apply(FakeRequest(GET, "/api/expenses/1"))
 
       status(home) mustBe OK
@@ -34,7 +32,7 @@ class ExpenseControllerSpec extends PlaySpec with GuiceOneAppPerTest with Inject
 
     "respond with NotFound when id provided is unavailable" in {
       val repo = stub[ExpenseRepository]
-      val controller = new ExpenseController(repo, stubControllerComponents())
+      val controller = new ExpenseController(stubControllerComponents(), repo)
       (repo.getExpenseById(_: Int)).when(*).returns(Future.successful(None))
 
       val home = controller.getExpense(-1).apply(FakeRequest(GET, "/api/expenses/-1"))
@@ -49,10 +47,11 @@ class ExpenseControllerSpec extends PlaySpec with GuiceOneAppPerTest with Inject
   "ExpenseController#getExpensesBetween" should {
     "respond with OK to a valid request " in {
       val repo = stub[ExpenseRepository]
-      val controller = new ExpenseController(repo, stubControllerComponents())
       val expenses = List(Expense(1,"Desc",10.0,LocalDate.parse("2000-01-01"),"Rewe"),
         Expense(2,"Desc",10.0,LocalDate.parse("2000-01-01"),"Rewe"))
       (repo.getExpensesBetween(_: LocalDate, _: LocalDate)).when(*,*).returns(Future.successful(expenses))
+
+      val controller = new ExpenseController(stubControllerComponents(), repo)
 
       val home = controller.getExpensesBetween("01/01/2000","01/01/2000").apply(FakeRequest())
 
